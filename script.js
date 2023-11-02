ec2db84e4538f924d74e06482a953238ca63fff5
Julie <j.prunaret@gmail.com>
2023-11-02 12:21:17 +0100

    feature - calling api successfully

---
+++ script.js
@@ -0,0 +1,14 @@
+const API_KEY = "039a6308d88f4abb97f9c37ac8b661cf";
+const url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${API_KEY}`;
+
+const fetchMoviesJSON = async () => {
+  const response = await fetch(url);
+  const articles = await response.json();
+  return articles;
+};
+
+fetchMoviesJSON().then((articles) => {
+  articles = articles.articles;
+  console.log(articles); // fetched artciles
+  articles.slice(0, 10).map((article) => console.log(article));
+});
