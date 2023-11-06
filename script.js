827905d3684bc2ae6a5dd525b530313d225d225e
Julie <j.prunaret@gmail.com>
2023-11-06 12:03:56 +0100

    feature - custom datas from news api

--- script.js
+++ script.js
@@ -11,10 +11,36 @@ const fetchMoviesJSON = async () => {
 fetchMoviesJSON().then((articles) => {
   articles = articles.articles;
   console.log(articles); // fetched artciles
-  articles.slice(0, 10).map((article) => {
-    theArticle = document.createElement("li");
-    articlesBox.appendChild(theArticle);
+  let articleNumber = 0;
+  articles.slice(0, 3).map((article) => {
+    // theArticle = document.createElement("li");
+    // articlesBox.appendChild(theArticle);
+    articleNumber += 1;
+    let description = article.description.substr(0, 50);
+    let title = article.title.substr(0, 20);
+    articlesBox.innerHTML += `
+    <li>
+      <img src="${article.urlToImage} alt="" />
+      <a href="${article.url}" target="_blank">
+        <h2>0${articleNumber}</h2>
+        <h3>${
+          article.title.length > 20
+            ? title.substr(0, Math.min(title.length, title.lastIndexOf(" "))) +
+              "..."
+            : article.title
+        }</h3>
+        <p>${
+          article.description.length > 50
+            ? description.substring(
+                0,
+                Math.min(description.length, description.lastIndexOf(" "))
+              ) + "..."
+            : article.description
+        }</p>
+      </a >
+    </li>
+    `;
 
-    theArticle.innerText = article.title;
+    // theArticle.innerText = article.title;
   });
 });
